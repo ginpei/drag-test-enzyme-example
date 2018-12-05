51ab7d0df2ad41df27a7fa3d137ccc9955ab6a21
ginpei <ginpei@gmail.com>
2018-12-04 16:23:22 -0800

    tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,18 @@
+import * as enzyme from 'enzyme';
+import { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+enzyme.configure({ adapter: new Adapter() });
+
+describe('<App>', () => {
+  it('renders finely', () => {
+    expect(() => {
+      const wrapper = shallow<App>(
+        <App/>,
+      );
+      expect(wrapper.hasClass('App')).toBeTruthy();
+    }).not.toThrow();
+  });
 });
